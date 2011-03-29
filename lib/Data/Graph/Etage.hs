8b5ba00b4d06c9419971656cecf8ef45e9a575e9
Mitar <mitar.git@tnode.com>
2011-03-29 03:10:01 +0200

    Testing and debuging.

--- lib/Data/Graph/Etage.hs
+++ lib/Data/Graph/Etage.hs
@@ -1,10 +1,11 @@
-{-# LANGUAGE TypeFamilies, MultiParamTypeClasses, ScopedTypeVariables, TypeSynonymInstances, StandaloneDeriving, DeriveDataTypeable, NamedFieldPuns #-}
+{-# LANGUAGE TypeFamilies, MultiParamTypeClasses, FlexibleInstances, ScopedTypeVariables, TypeSynonymInstances, StandaloneDeriving, DeriveDataTypeable, NamedFieldPuns #-}
 {-# OPTIONS_GHC -fno-warn-orphans #-}
 
 module Data.Graph.Etage (
   shortestPaths
 ) where
 
+import Control.Exception
 import Control.Monad.State
 import Data.Data
 import Data.Graph.Inductive hiding (inn, inn', out, out', node', nodes, run)
@@ -12,6 +13,7 @@ import qualified Data.Map as M
 import Data.Map hiding (filter, map, empty)
 import Data.Tuple
 import Control.Etage
+import System.IO
 
 type SPath b = (LPath b, b)
 type SPaths a b = M.Map Node (a, SPath b) -- node is destination, last element of SPath
@@ -25,10 +27,18 @@ shortestPaths from graph = do
 
 growGraph :: forall a b. (Show a, Data a, Data b, Real b, Bounded b) => Context a b -> M.Map Node (Nerve (GraphImpulse a b) AxonConductive (GraphImpulse a b) AxonConductive) -> Incubation (M.Map Node (Nerve (GraphImpulse a b) AxonConductive (GraphImpulse a b) AxonConductive))
 growGraph (inn, node, label, out) nodes = do
-  -- TODO: Add assertion check for node not being in nodes
-  nodeNerve <- (growNeuron :: NerveBoth (NodeNeuron a b)) (\o -> o { lnode = (node, label), linedges = inn' })
+  -- TODO: Sometimes nerve is not connected in both directions, how to fix memory leak then?
+  liftIO $ do
+    assertIO $ node `notMember` nodes
+    assertIO $ all ((`member` nodes) . snd) inn'
+    assertIO $ all ((`member` nodes) . snd) out'
+  nodeNerve <- (growNeuron :: NerveBoth (NodeNeuron a b)) (\o -> o { lnode = (node, label) })
   mapM_ ((`attachTo` [TranslatableFor nodeNerve]) . (nodes !) . snd) inn'
   nodeNerve `attachTo` map (TranslatableFor . (nodes !) . snd) out'
+  liftIO $ do
+    time <- getCurrentImpulseTime
+    sendForNeuron nodeNerve $ AddInEdges time inn'
+    mapM_ (\(l, n) -> sendForNeuron (nodes ! n) $ AddInEdges time [(l, node)]) out'
   return $ insert node nodeNerve nodes
     where inn' = filter ((node /=) . snd) $ inn -- we ignore loopbacks
           out' = filter ((node /=) . snd) $ out -- we ignore loopbacks
@@ -41,12 +51,13 @@ sendTopologyChange nerve = liftIO $ do
 
 data NodeState a b = NodeState {
     lastTopologyChangeTimestamp :: ImpulseTime,
-    currentPaths :: SPaths a b
+    currentPaths :: SPaths a b,
+    inedges :: M.Map Node b
   }
 
 type NodeIO a b = StateT (NodeState a b) IO
 
-data NodeNeuron a b = NodeNeuron Node a (M.Map Node b) deriving (Typeable, Data)
+data NodeNeuron a b = NodeNeuron Node a deriving (Typeable, Data)
 
 deriving instance Typeable1 LPath
 deriving instance Data a => Data (LPath a)
@@ -59,53 +70,71 @@ data GraphImpulse a b = TopologyUpdate {
   } |
   TopologyChange {
     impulseTimestamp :: ImpulseTime
+  } |
+  AddInEdges {
+    impulseTimestamp :: ImpulseTime,
+    newInEdges :: Adj b
   } deriving (Eq, Ord, Show, Typeable, Data)
 
 instance (Show a, Typeable a, Show b, Typeable b, Real b, Bounded b) => Impulse (GraphImpulse a b) where
   impulseTime TopologyUpdate { impulseTimestamp } = impulseTimestamp
   impulseTime TopologyChange { impulseTimestamp } = impulseTimestamp
+  impulseTime AddInEdges { impulseTimestamp } = impulseTimestamp
   impulseValue TopologyUpdate { originator, path } = (toRational o) : (value . fst $ path)
     where (o, _) = originator
           value (LP p) = concatMap (\(n, l) -> [toRational n, toRational l]) p
   impulseValue TopologyChange {} = []
+  impulseValue AddInEdges { newInEdges } = concatMap (\(l, n) -> [toRational l, toRational n]) newInEdges
 
 instance (Show a, Data a, Show b, Data b, Real b, Bounded b) => Neuron (NodeNeuron a b) where
   type NeuronFromImpulse (NodeNeuron a b) = GraphImpulse a b
   type NeuronForImpulse (NodeNeuron a b) = GraphImpulse a b
   data NeuronOptions (NodeNeuron a b) = NodeOptions {
-      lnode :: LNode a,
-      linedges :: Adj b
+      lnode :: LNode a
     } deriving (Eq, Ord, Read, Show) -- TODO: Derive Data when it will work
 
-  grow NodeOptions { lnode = (node, label), linedges } = return $ NodeNeuron node label linedges'
-    where linedges' = fromList . map swap $ linedges
+  mkDefaultOptions = return NodeOptions {
+      lnode = undefined
+    }
+
+  grow NodeOptions { lnode = (node, label) } = return $ NodeNeuron node label
   
-  live nerve neuron@(NodeNeuron node label _) = evalStateT (run nerve neuron) (NodeState 0 (singleton node (label, (LP [(node, 0)], 0))))
+  live nerve neuron@(NodeNeuron node label) = evalStateT (run nerve neuron) (NodeState 0 (singleton node (label, (LP [(node, 0)], 0))) M.empty)
 
 run :: (Data b, Real b, Bounded b) => Nerve (GraphImpulse a b) fromConductivity (GraphImpulse a b) forConductivity -> NodeNeuron a b -> NodeIO a b ()
-run nerve (NodeNeuron node label inedges) = forever $ do
-  i <- liftIO $ getForNeuron nerve
-  case i of
+run nerve (NodeNeuron node label) = forever $ do
+  impulse <- liftIO $ getForNeuron nerve
+  case impulse of
     TopologyChange { impulseTimestamp } -> do
       lastTimestamp <- gets lastTopologyChangeTimestamp
       when (impulseTimestamp > lastTimestamp) $ do
         modify (\s -> s { lastTopologyChangeTimestamp = impulseTimestamp })
         paths <- gets currentPaths
         liftIO $ do
-          sendFromNeuron nerve i
-          t <- getCurrentImpulseTime
+          sendFromNeuron nerve impulse
+          t <- liftIO getCurrentImpulseTime
           forM_ (toList paths) $ \(n, (l, p)) -> sendFromNeuron nerve TopologyUpdate { impulseTimestamp = t, originator = (node, label), destination = (n, l), path = p }
     TopologyUpdate { impulseTimestamp, originator = (o, _), destination = (d, l), path = (LP path, cost) } -> do
-      -- TODO: Add assertion which checks if path length is really same as cost
-      paths <- gets currentPaths
-      let (_, (_, c)) = findWithDefault (undefined, (undefined, maxBound)) d paths
-          ocost = inedges ! o
-          cost' = cost + ocost
-      when (cost' < c) $ do
-        let path' = LP $ (o, ocost) : path
-            paths' = insert d (l, (path', cost')) paths
-        modify (\s -> s { currentPaths = paths' })
-        liftIO $ sendFromNeuron nerve TopologyUpdate { impulseTimestamp, originator = (node, label), destination = (d, l), path = (path', cost') }
+      liftIO $ do
+        assertIO $ cost == (sum . map snd $ path)
+        assertIO $ (fst . last $ path) == d
+      inn <- gets inedges
+      if o `notMember` inn
+        then liftIO $ hPutStrLn stderr $ "Warning: TopologyUpdate message arrived before AddInEdges message."
+        else do
+          paths <- gets currentPaths
+          let (_, (_, c)) = findWithDefault (undefined, (undefined, maxBound)) d paths
+              ocost = inn ! o
+              cost' = cost + ocost
+          when (cost' < c) $ do
+            let path' = LP $ (o, ocost) : path
+                paths' = insert d (l, (path', cost')) paths
+            modify (\s -> s { currentPaths = paths' })
+            liftIO $ sendFromNeuron nerve TopologyUpdate { impulseTimestamp, originator = (node, label), destination = (d, l), path = (path', cost') }
+    AddInEdges { newInEdges } -> do
+      inn <- gets inedges
+      let inn' = foldl (\i (l, n) -> insert n l i) inn newInEdges
+      modify (\s -> s { inedges = inn' })
 
 ufoldM' :: (Graph gr, Monad m) => (Context a b -> c -> m c) -> c -> gr a b -> m c
 ufoldM' f u g | isEmpty g = return u
@@ -117,3 +146,13 @@ gmapM' :: (DynGraph gr, Monad m) => (Context a b -> m (Context c d)) -> gr a b -
 gmapM' f = ufoldM' (\c u -> f c >>= \c' -> return $ c' & u) empty
 -}
 
+instance Bounded Float where
+  minBound = -1/0
+  maxBound = 1/0
+
+instance Bounded Double where
+  minBound = -1/0
+  maxBound = 1/0
+
+assertIO :: Bool -> IO ()
+assertIO cond = evaluate (assert cond ())
---
+++ src/Test.hs
@@ -0,0 +1,39 @@
+module Main (
+  main
+) where
+
+import Control.Monad
+import Control.Exception
+import Data.List
+import System.Random
+
+import Control.Etage
+import Control.Etage.Dump
+import Data.Graph.Etage
+import Data.Graph.Inductive hiding (nodes, edges)
+
+outputFilename :: String
+outputFilename = "out.dot"
+
+generateGraph :: Int -> IO (Gr Char Float)
+generateGraph size = do
+  when (size < 1 || size > 26) $ throwIO $ AssertionFailed $ "Graph size out of bounds " ++ show size
+  let nodes = zip [1..size] ['A'..]
+  edges <- fmap concat $ forM [1..size] $ \node -> do
+    nedges <- randomRIO (0, size)
+    others <- fmap (filter (node /=) . nub) $ forM [1..nedges] $ \_ -> randomRIO (1, size)
+    gen <- getStdGen
+    let weights = randomRs (1, 10) gen
+    return $ zip3 (repeat node) others weights
+  return $ mkGraph nodes edges
+
+main :: IO ()
+main = do
+  graph <- generateGraph 6
+  putStrLn $ "Writing graph in dot format to \"" ++ outputFilename ++ "\"."
+  writeFile outputFilename $ graphviz graph "Etage" (8.5, 11) (1, 1) Portrait
+  incubate $ do
+    nervePaths <- shortestPaths 1 graph
+    nerveDump <- (growNeuron :: NerveOnlyFor DumpNeuron) (\o -> o { showInsteadOfDump = True })
+    
+    nervePaths `attachTo` [TranslatableFor nerveDump]
