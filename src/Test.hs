5a209f43280de5e45b4e52812c9e9829b43bc170
Mitar <mitar.git@tnode.com>
2011-04-02 12:45:29 +0200

    Options parsing.

--- src/Test.hs
+++ src/Test.hs
@@ -9,21 +9,59 @@ import Control.Monad
 import Control.Exception
 import Data.Data
 import Data.Graph.Etage
-import Data.Graph.Inductive hiding (nodes, edges)
+import Data.Graph.Inductive hiding (nodes, edges, defaultGraphSize)
 import Data.List
 import qualified Data.Map as M
 import Data.Map hiding (filter, map, empty, null, lookup, insert)
+import Data.Maybe
+import System.Console.GetOpt
 import System.Environment
+import System.Exit
 import System.Random
+import System.IO
 import Text.Printf
 
 import Control.Etage
 
-outputGraphFilename :: String
-outputGraphFilename = "out.graph"
+defaultGraphSize :: Int
+defaultGraphSize = 6
 
-outputDotFilename :: String
-outputDotFilename = "out.dot"
+data Option = InputGraph String | OutputGraph String | OutputDot String | SearchNode Node | GraphSize Int | Help deriving (Eq, Show)
+
+options :: [OptDescr Option]
+options = [
+    Option ['g'] ["graph"]  (ReqArg InputGraph "filename")             "read graph grom a file, default is to generate one randomly",
+    Option ['o'] ["output"] (ReqArg OutputGraph "filename")            "save graph to a file",
+    Option ['d'] ["dot"]    (ReqArg OutputDot "filename")              "save graph to a file in a GraphViz format",
+    Option ['n'] ["node"]   (ReqArg (SearchNode . readParam) "number") "index of the node to search shortest paths from, default is to pick one randomly",
+    Option ['s'] ["size"]   (ReqArg (GraphSize . readParam) "number")  ("size of the randomly generated graph, default is " ++ (show defaultGraphSize)),
+    Option ['h'] ["help"]   (NoArg Help)                               "show this help"
+  ]
+    where readParam param = case reads param of
+                              [(p,[])] -> if p < 1
+                                            then error $ "invalid parameter `" ++ param ++ "'"
+                                            else p
+                              _        -> error $ "invalid parameter `" ++ param ++ "'"
+
+isInputGraph :: Option -> Bool
+isInputGraph (InputGraph _) = True
+isInputGraph _              = False
+
+isGraphSize :: Option -> Bool
+isGraphSize (GraphSize _) = True
+isGraphSize _             = False
+
+isOutputGraph :: Option -> Bool
+isOutputGraph (OutputGraph _) = True
+isOutputGraph _               = False
+
+isOutputDot :: Option -> Bool
+isOutputDot (OutputDot _) = True
+isOutputDot _             = False
+
+isSearchNode :: Option -> Bool
+isSearchNode (SearchNode _) = True
+isSearchNode _              = False
 
 generateGraph :: Int -> IO (Gr String Double)
 generateGraph graphSize = do
@@ -73,23 +111,46 @@ main = do
   prepareEnvironment
   
   args <- getArgs
+  opts <- case getOpt Permute options args of
+            (o, [], [])  -> return o
+            (_, p:_, []) -> throwIO $ ErrorCall $ "unrecognized option `" ++ p ++ "'"
+            (_, _, errs) -> throwIO $ ErrorCall $ head $ lines $ head errs
+
+  when (Help `elem` opts) $ do
+    programName <- getProgName
+    putStrLn $ "Usage:\n" ++ usageInfo (programName ++ " [option ...]" ++ "\n\nOptions:") options
+    exitWith ExitSuccess
   
-  graph <- if null args
-             then do
-               graph <- generateGraph 6
-               putStrLn $ "Writing graph to \"" ++ outputGraphFilename ++ "\"."
-               writeFile outputGraphFilename $ (show . labNodes $ graph) ++ "\n" ++ (show . labEdges $ graph) ++ "\n"
-               return graph
-             else do
-               putStrLn $ "Reading graph from \"" ++ (head args) ++ "\"."
-               [line1, line2] <- lines <$> (readFile $ head args)
-               return $ mkGraph (read line1) (read line2)
+  (graph, graphSize) <- case find isInputGraph opts of
+                          Just (InputGraph inputGraph) -> do
+                            when (any isGraphSize opts) $ throwIO $ ErrorCall $ "conflicting options `" ++ "--graph" ++ "' and `" ++ "--size" ++ "'"
+                            putStrLn $ "Reading graph from \"" ++ inputGraph ++ "\"."
+                            [line1, line2] <- lines <$> (readFile inputGraph)
+                            let g = mkGraph (read line1) (read line2)
+                            return (g, noNodes g)
+                          _                            -> do
+                            let GraphSize s = fromMaybe (GraphSize defaultGraphSize) $ find isGraphSize opts
+                            putStrLn $ "Generating a random graph of size " ++ (show s) ++ "."
+                            g <- generateGraph s
+                            return (g, s)
+
+  case find isOutputGraph opts of
+    Just (OutputGraph outputGraph) -> do
+      putStrLn $ "Writing graph to \"" ++ outputGraph ++ "\"."
+      writeFile outputGraph $ (show . labNodes $ graph) ++ "\n" ++ (show . labEdges $ graph) ++ "\n"
+    _                              -> return ()
+
+  case find isOutputDot opts of
+    Just (OutputDot outputDot) -> do
+      putStrLn $ "Writing graph in dot format to \"" ++ outputDot ++ "\"."
+      writeFile outputDot $ graphviz graph "Etage" (8.27, 11.69) (1, 1) Landscape
+    _                          -> return ()
   
-  putStrLn $ "Writing graph in dot format to \"" ++ outputDotFilename ++ "\"."
-  writeFile outputDotFilename $ graphviz graph "Etage" (11.69, 8.27) (1, 1) Portrait
-  
-  let graphSize = noNodes graph
-  sourceNode <- randomRIO (1, graphSize)
+  sourceNode <- case find isSearchNode opts of
+                  Just (SearchNode s) -> do
+                    when (s < 1 || s > graphSize) $ throwIO $ ErrorCall $ "invalid parameter `" ++ (show s) ++ "'"
+                    return s
+                  _                   -> randomRIO (1, graphSize)
   
   putStrLn $ "Graph contains " ++ (show graphSize) ++ " nodes. Searching for shortest paths from node " ++ (show sourceNode) ++ "."
   
